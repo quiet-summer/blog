831b36a44e44cd32bff00a57b289a8930a8838e4
bestjane <mr.bestjane@gmail.com>
2016-07-26 22:32:43 +0800

    修复登录成功后的跳转页面

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,12 @@ class ApplicationController < ActionController::Base
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+
   before_action :authenticate_user!
+
+  #登录成功后
+  def after_sign_in_path_for(resource_or_scope)
+   admin_root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
 
   root 'home#index'
+
   devise_for :users
+
   namespace :admin do
     root 'home#index'
     resources :articles
