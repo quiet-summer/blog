d68726cb4841f51fba3fe0cdc0e9a969c91989f6
bestjane <mr.bestjane@gmail.com>
2016-07-26 22:37:52 +0800

    添加默认测试数据

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 User.create(email: 'admin@admin.com', password: '12345678')
+100.times.each{|i| Article.create(title:"标题#{i}", content: "内容#{i}") }
